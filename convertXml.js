0ae4608cdd70b6c85abf9da6747347a51b31f2bf
soh92 <pix4soh@gmail.com>
2019-09-17 22:25:58 +0900

    実証っぽいのはできたかも

---
+++ convertXml.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const che = require('cheerio');
+const kuromoji = require('kuromoji');
+
+// 文中の空白を削除する関数
+function dispAry(text) {
+  return text.replace(/\s+/g, "");
+}
+
+postArry = ['代表取締役', '支配人', '社員', '代表社員'];
+
+// XMLファイル読み込み
+const xml_data = fs.readFileSync("from.xml", "utf-8");
+const $ = che.load(xml_data);
+
+// XMLの「name」タグを読み込み
+const xmlName = dispAry($('name').text());
+// console.log(dispAry(name));
+
+// 形態素解析のオブジェクト
+const builder = kuromoji.builder({
+  // 辞書を設定
+  dicPath: 'node_modules/kuromoji/dict'
+});
+
+// 形態素解析機のビルド
+builder.build(function(err, tokenizer) {
+  if(err) { throw err; }
+
+  const tokens = tokenizer.tokenize(xmlName);
+
+  // 名前を取り出す
+  const firstName = tokens.find(text => {
+    return text.pos_detail_3 === '名'
+  });
+
+  // 姓を取り出す
+  const lastName = tokens.find(text => {
+    return text.pos_detail_3 === '姓'
+  });
+
+  // 名前の結合
+  const name = lastName.basic_form + firstName.basic_form;
+
+  // 役職を取り出す
+  let postName = '';
+  postArry.forEach(post => { 
+    if (xmlName.indexOf(post) !== -1) {
+      postName = post;
+    }
+  });
+  // xmlNameの調整
+  console.log(postName);
+  console.log(name);
+});
